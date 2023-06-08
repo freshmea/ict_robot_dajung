54770639f6924f5985d6d792f89ac68a18b5061d
freshmea <freshmea@naver.com>
2023-06-08 05:03:25 -0700

    class15

---
+++ src/class14.py
@@ -0,0 +1,47 @@
+def main():
+    a = 'choisugil'
+    b = 'CHOISUGIL'
+    print(a.upper())
+    print(b.lower())
+    
+    c = '    choi    '
+    print(c+'end')
+    print(c.strip()+'end')
+    
+    d = 'this is a python class in ict'
+    print(d.find('a'))
+    print(d[d.find('a')])
+    print(d.rfind('a'))
+    
+    idx = int()
+    idx2 = int()
+    while(d.find('a', idx)):
+        print(idx)
+        idx = d.find('a', idx)+1
+        if idx == idx2 :
+            break
+        idx2 = d.find('a', idx)+1
+
+    f = 'TrainA10'.isalnum()
+    print(f)
+    
+    f = 'Train_A10!'.isalnum()
+    print(f)
+    
+    f = '10'.isdigit()
+    print(f)
+    
+    f = '10a'.isdigit()
+    print(f)
+    
+    g = 'this is python class'.split()
+    print(g[0])
+    print(g[1])
+    print(g[2])
+    print(g[3])
+    
+    print('안녕' in '안녕하세요')
+    print('요요' in '안녕하세요')
+    
+if __name__ == '__main__':
+    main()
---
+++ src/class15.py
@@ -0,0 +1,43 @@
+def main():
+    # 비교 연산자 연습
+    print(10 == 100) # False
+    print(10 != 100) # True
+    print(10 < 100) # True
+    print(10 > 100) # False
+    print(10 <= 100) # True
+    print(100 <= 100) # True
+    print(100 < 100) # False
+    
+    print('\n\n\n-------------------------------구분선')
+    # 논리 연산자
+    print(not True) # False
+    print(not (10 < 100)) # False
+    
+    # 무족건 실행
+    if True:
+        print('True 입니다...!')
+    # 무족건 실행 안됨
+    if False:
+        print('False 입니다...!')
+        
+    a = int(input('100보다 큰 숫자를 넣으세요:'))
+    
+    # if a >100:
+    #     print('입력한 숫자가 100보다 큽니다.')
+    # if a <= 100:
+    #     print('입력한 숫자가 100보다 작거나 같습니다.')
+        
+    # if a >100:
+    #     print('입력한 숫자가 100보다 큽니다.')
+    # else:
+    #     print('입력한 숫자가 100보다 작거나 같습니다.')
+        
+    if a > 200:
+        print('입력한 숫자가 200보다 큽니다.')
+    elif a > 100:
+        print('입력한 숫자가 100보다 크고 200보다 작습니다.')
+    else:
+        print('입력한 숫자가 100보다 작거나 같습니다.')
+
+if __name__ == '__main__':
+    main()
