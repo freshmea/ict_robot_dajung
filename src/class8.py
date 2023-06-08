a1d456cf6ce008fb3c8ad6c4d418f54a3554d3fa
freshmea <freshmea@naver.com>
2023-06-08 02:47:03 -0700

    class8

---
+++ src/class8.py
@@ -0,0 +1,21 @@
+def main():
+    print(123)
+    print(123.456)
+    print(type(123))
+    print(type(123.456))
+    print(2 ** 2)
+    print(2 ** 64)
+    
+    # 나누기 연산
+    print( 18/3)
+    print(type(18/3))
+    
+    print(18//3)
+    print(type(18//3))
+    
+    print(14/3)
+    print(14//3)
+    print(14 % 3)
+    
+if __name__ == '__main__':
+    main()
