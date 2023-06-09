6d59290a9015429f1ceae639f38bbe0bdb1347f1
freshmea <freshmea@naver.com>
2023-06-09 02:24:42 -0700

    class21

---
+++ src/class21.py
@@ -0,0 +1,25 @@
+import datetime
+
+def main():
+    # 리스트 변수 만들기1
+    list_a = []
+    # 리스트 변수 만들기2
+    list_b = list()
+    print(list_b)
+    print(type(list_b))
+
+    
+    ptime = datetime.datetime.now()
+    print(type(ptime))
+    list_c = [1, 2, 3.3, 'choi', 'su', 'gil', ptime]
+    
+    print(list_c[3])
+    print(list_c[-1])
+
+    # 이중 리스트
+    list_d =[[1,2,3], [4,5,6], [7,8,9]]
+    print(list_d[1])
+    print(list_d[1][2])
+    # print(list_d[3])  # out of index
+if __name__ == '__main__':
+    main()
