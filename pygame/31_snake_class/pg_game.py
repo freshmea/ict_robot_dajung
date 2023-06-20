e58055273f29de2b3339f94b9d2e0d52d45f0d0e
freshmea <freshmea@naver.com>
2023-06-20 06:08:33 -0700

    snake_class2

--- pygame/31_snake_class/pg_game.py
+++ pygame/31_snake_class/pg_game.py
@@ -1,6 +1,55 @@
-import pygame, sys
+import pygame, sys, random
 from pg_local import *
 from pg_text import Text
+vec = pygame.math.Vector2
+
+class Snake(pygame.sprite.Sprite):
+    def __init__(self, root, head):
+        super().__init__()
+        self.game = root
+        self.length = 1
+        self.image = pygame.Surface((GRID,GRID))
+        self.rect = self.image.get_rect()
+        self.color1 = pygame.Color('green')
+        self.color2 = pygame.Color('green4')
+        self.head_color = pygame.Color('crimson')
+        self.head = head
+        self.create_snake()
+
+    def create_snake(self):
+        self.length = 3
+        self.rect.topleft =(GRID*15, GRID*15)
+        self.direction = random.choice([(-1,0),(1,0),(0, -1),(0, 1)])
+        self.game.score = 0
+        if self.head:
+            self.image.fill(self.head_color)
+        elif self.game.color_check:
+            self.image.fill(self.color1)
+            self.game.color_check = False
+        else:
+            self.image.fill(self.color2)
+            self.game.color_check = True
+    
+    def change_dir(self, dir):
+        self.direction = dir
+        
+    def update(self):
+        print(self.game.snakes.index(self), self.rect)
+        if self.head:
+            self.game.t_rect[0] = self.rect.copy()
+            self.rect.topleft += vec(self.direction)*GRID
+            if self.rect.x >= SCREEN_X:
+                self.rect.x = 0
+            if self.rect.x < 0:
+                self.rect.x = SCREEN_X
+            if self.rect.y >= SCREEN_Y:
+                self.rect.y = 0
+            if self.rect.y < 0:
+                self.rect.x = SCREEN_Y
+        else:
+            self.game.t_rect[self.game.snakes.index(self)]=self.rect.copy()
+            self.rect = self.game.t_rect[self.game.snakes.index(self)-1]
+            
 
 class Game:
     def __init__(self):
@@ -9,14 +58,22 @@ class Game:
         self.clock = pygame.time.Clock()
         self.playing = True
         self.replay = True
+        self.score = 0
+        self.color_check = True
+        self.all_sprites = pygame.sprite.Group()
+        snake = Snake(self, True)
+        self.all_sprites.add(snake)
+        self.snakes =[snake]
+        self.t_rect = [pygame.Rect(GRID,GRID,GRID,GRID)]
 
     def run(self):
         self.opening()
         self.playing = True
         while self.playing:
             self.clock.tick(FPS)
-            self.event()
             self.update()
+            self.event()
+            self.make_snake()
             self.draw()
         self.playing = True
         self.ending()
@@ -53,13 +110,30 @@ class Game:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     self.playing = False
+                if event.key == pygame.K_UP:
+                    self.all_sprites.sprites()[0].change_dir((0,-1))
+                if event.key == pygame.K_DOWN:
+                    self.all_sprites.sprites()[0].change_dir((0,1))
+                if event.key == pygame.K_RIGHT:
+                    self.all_sprites.sprites()[0].change_dir((1,0))
+                if event.key == pygame.K_LEFT:
+                    self.all_sprites.sprites()[0].change_dir((-1,0))
+
+    def make_snake(self):
+        # 스네이크 만드는 클래스.
+        if len(self.all_sprites) <= self.snakes[0].length:
+            snake = Snake(self, False)
+            self.all_sprites.add(snake)
+            self.snakes.append(snake)
+            self.t_rect.append(pygame.Rect(GRID,GRID,GRID,GRID))
 
     def update(self):
-        pass
+        self.all_sprites.update()
 
     def draw(self):
         self.screen.fill(pygame.Color('white'))
         self.draw_grid()
+        self.all_sprites.draw(self.screen)
         pygame.display.update()
         
     def draw_grid(self):
