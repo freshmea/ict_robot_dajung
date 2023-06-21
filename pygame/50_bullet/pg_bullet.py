3c022c94f32d56a36282d8856b860289c6497dad
freshmea <freshmea@naver.com>
2023-06-21 05:52:38 -0700

    bullet

---
+++ pygame/50_bullet/pg_bullet.py
@@ -0,0 +1,20 @@
+import pygame
+from pg_local import *
+vec = pygame.math.Vector2
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, root, vel):
+        super().__init__()
+        self.game = root
+        self.image = pygame.Surface((25,25))
+        self.rect = self.image.get_rect()
+        self.image.fill((255,0,0))
+        self.pos = vec(0,SCREEN_Y)
+        self.rect.center = self.pos
+        self.vel = vel*100
+        self.gravity = 0.01
+    
+    def update(self):
+        self.vel.y += self.gravity
+        self.pos += self.vel
+        self.rect.center = self.pos
