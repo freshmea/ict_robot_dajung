5a736e9dd153167d7826a77d1cb26f77a829e6a8
freshmea <freshmea@naver.com>
2023-06-23 02:41:35 -0700

    plt video

--- opencv/class04.py
+++ opencv/class04.py
@@ -1,9 +1,11 @@
 import cv2
 
-cap = cv2.VideoCapture('opencv/data/vtest.avi')
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+# cap.set(cv2.CAP_PROP_BITRATE, )
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+print(cap.get(cv2.CAP_PROP_BITRATE))
 frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 print('frame_size =', frame_size)
 
