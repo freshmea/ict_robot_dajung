fc9d6b20a6744a6e981fd571754698289a3c56be
freshmea <freshmea@naver.com>
2023-06-29 05:11:51 -0700

    hog2

--- opencv/class65.py
+++ opencv/class65.py
@@ -1,14 +1,23 @@
 import cv2 
 import numpy as np 
-from matplotlib import pyplot as plt
-src = cv2.imread('opencv/data/people1.png')
-src2 = cv2.imread('opencv/data/people.png')
 
-hog1 = cv2.HOGDescriptor()
-des1 = hog1.compute(src)
+
+src = cv2.imread('opencv/data/people.png')
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+loc1, weights1 = hog.detectMultiScale(src)
+print(len(loc1))
+dst1 = src.copy()
+w, h = hog.winSize
+for pt in loc1:
+    x, y, w, h= pt
+    cv2.rectangle(dst1, (x,y), (x+w, y+h),(255,0,0), 2)
+
 
 cv2.imshow('src', src)
-cv2.imshow('src2', src2)
+cv2.imshow('dst1', dst1)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
