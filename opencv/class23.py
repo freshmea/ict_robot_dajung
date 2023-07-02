94764729259490abf656c888c71a34a7682e7dd5
freshmea <freshmea@naver.com>
2023-07-03 00:00:15 +0900

    aa

--- opencv/class23.py
+++ opencv/class23.py
@@ -6,7 +6,7 @@ img = cv2.imread('opencv/data/lena.jpg', cv2.IMREAD_GRAYSCALE)  # Numpy.array --
 print('img.shape= ', img.shape)
 img = img.flatten()
 print('img.shape= ', img.shape)
-img = img.reshape( 512, -1) #(512, 512)
+img = img.reshape(512, 512) #(512, 512)
 # 윈도우 (-1, 512, 512)
 print('img.shape= ', img.shape)
 
