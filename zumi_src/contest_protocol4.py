cfec9b48c9de94736601f11d0f3e747cc83d9fe3
freshmea <freshmea@naver.com>
2023-07-25 20:09:09 +0900

    aa

--- zumi_src/contest_protocol4.py
+++ zumi_src/contest_protocol4.py
@@ -40,12 +40,12 @@ def happymove():
     for _ in range(3):
         zumi.turn(init_angle+20, 0.5)
         zumi.turn(init_angle-20, 0.5)
-        
+
 def doremi():
     zumi.play_note(Note.C3, 1000)
     zumi.play_note(Note.D3, 1000)
     zumi.play_note(Note.E3, 1000)
-    
+
 def find_line(speed, rotate):
     heading = zumi.read_z_angle()
     while True:
@@ -57,8 +57,10 @@ def find_line(speed, rotate):
             break
         zumi.drive_at_angle(10, speed, heading, zumi.D_P, zumi.D_I, zumi.D_D, 1.0)
 
-# A-1,2,3,4
+
 try:
+    # A-1
+    print('A-1 start')
     camera.start_camera()
     qr = False
     while not qr:
@@ -66,6 +68,8 @@ try:
         qr = vision.find_QR_code(frame)
     doremi()
     message = vision.get_QR_message(qr)
+    # A-2
+    print('A-2 start')
     if message == 'factory':
         zumi.drive_over_markers(4, time_out=5)
     if message == 'school':
@@ -74,7 +78,8 @@ try:
         zumi.drive_over_markers(12, time_out=5)
     if message == 'museum':
         zumi.drive_over_markers(16, time_out=5)
-    
+    # A-3
+    print('A-3 start')
     if message == 'factory' or message == 'office':
         zumi.turn(desired_angle= -90, duration= 3, max_speed=10)
     if message == 'school' or message == 'museum':
@@ -87,10 +92,13 @@ try:
     zumi.forward(30, 1.4)
     zumi.turn(desired_angle= 0, duration= 3, max_speed=10)
     zumi.forward_avoid_collision(speed=40, duration=10, desired_angle= 0, left_th=50, right_th= 50 )
+    # A-4
+    print('A-4 start')
     zumi.turn(-90)
     zumi.funnel_align(speed=20, duration=18)
     zumi.turn(-90)
     # B-1
+    print('B-1 start')
     while True:
         frame = camera.capture()
         if vision.detect_stop_sign(frame):
@@ -100,7 +108,8 @@ try:
         frame = camera.capture()
         if not vision.detect_stop_sign(frame):
             break
-    # B-2 
+    # B-2
+    print('B-2 start')
     heading = zumi.read_z_angle()
     b2_time = time.time()
     for x in range(350):
@@ -116,12 +125,13 @@ try:
             zumi.turn(heading, 0.5)
         else:
             zumi.forward_step(20, heading)
+    # B-3
+    print('B-3 start')
     zumi.turn(-90)
     zumi.funnel_align(speed=20, duration=6)
     zumi.turn(-90)
-    
-    # C-1 
-    camera.start_camera()
+    # C-1
+    print('C-1 start')
     user_name = 'aa'
     demo_name = 'red_yellow_green2'
     knn = ColorClassifier(user_name = user_name)
@@ -139,6 +149,7 @@ try:
         if predict == 'green':
             break
     # C-2
+    print('C-2 start')
     zumi.forward(10, 0.3)
     find_line(10, 0)
     zumi.line_follow_gyro_assist(speed=20, duration=2)
@@ -149,6 +160,7 @@ try:
     zumi.turn(180)
     zumi.line_follow_gyro_assist(speed=20, duration=2)
     # C-3
+    print('C-3 start')
     qr = False
     while not qr:
         frame = camera.capture()
@@ -159,10 +171,6 @@ try:
         zumi.turn(90)
     if message2 == 'left':
         zumi.turn(-90)
-
-    # TODO 회전시 선을 인식하지 못하면 앞으로 코스를 따라가지 않음.
-    # 회전 후에 라인을 인식할 수 있게 앞뒤 또는 좌우로 살짝 움직임이 필요.
-    # 보통 너무 앞쪽에 있음.(뒤로 움직이면서 라인을 인식하는 코드 필요.)
     find_line(-10, 0)
     zumi.line_follow_gyro_assist(speed=20, duration=3)
     zumi.turn(180)
@@ -180,9 +188,8 @@ try:
     screen.clear_drawing()
     screen.print(message,x= 0, y = 20, font_size= 20)
     screen.print(message2,x= 0, y = 40, font_size= 20)
-    
-    
-        
+
+
 except KeyboardInterrupt:
     zumi.stop()
     camera.close()
